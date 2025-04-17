8c4fd9b68f1663b734ac72198e68c20d88949b52
Codeur-Omniscient <anthonymedin258@outlook.fr>
2025-04-18 00:19:16 +0100

    Suppression du fichier .env.example et décommentaire du code dans route.ts pour activer la récupération des factures(fin chap 6)

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,9 @@ export default function Page() {
           >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
+              {" "}
+              Next.js Learn Course{" "}
+            </a>{" "}
             , brought to you by Vercel.
           </p>
           <Link
